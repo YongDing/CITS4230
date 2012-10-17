4bf1e750d92741ddc11943c9bbf8a374d8bdde33
YongDing <dytack0815@gmail.com>
2012-10-17 21:48:31 +0800

    start project

---
+++ picbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picbook::Application.config.secret_token = 'cd3c695f247efa53d6f8f8dbf20b2577864669a5d0e54c7c98990e837a4b92ff450149137d544760209dbf92cc1404f64ed31ee3317a76bc441d1da7d0110128'
