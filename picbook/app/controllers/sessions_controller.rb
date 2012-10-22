415dd4ef3e36b67c751f0c325f9c01d7293276ec
Showyilu <xiaoyi21b@hotmail.com>
2012-10-22 19:41:55 +0800

    jk

--- picbook/app/controllers/sessions_controller.rb
+++ picbook/app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 class SessionsController < ApplicationController
-  skip_before_filter :authorize
-  def new
-  end
+    skip_before_filter :authorize
+    def new
+    end
 
     def create
-        user = User.find_by_email(params[:email])
+        user = User.where("email = ?", params[:email])
         if user and user.authenticate?(params[:password])
             session[:user_id] = user.id
             redirect_to user_url(session[:user_id])
-            else
+        else
             redirect_to login_url, :alert=> "Password/User name incorrect"
         end
+        
     end
     
     def destroy
--- picbook/app/models/user.rb
+++ picbook/app/models/user.rb
@@ -1,4 +1,4 @@
-require 'digest/sha2'
+
 class User < ActiveRecord::Base
     attr_accessible :birthday, :email, :firstName, :gender, :lastName, :password
     validates_confirmation_of :password
@@ -11,28 +11,14 @@ class User < ActiveRecord::Base
     validates :email, :uniqueness => true
     validates_format_of  :email, :message => "has invalid format!", :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
     
-    before_save :encrypt_password
+    
     
     def authenticate?(submitted_password)
-		encrypted_password == encrypt(submitted_password)
+		if :password == submitted_password
+            true
+        else
+            false
+        end
     end
     
-    private
-  	
-    def encrypt_password
-		self.salt = make_salt if new_record?
-		self.encrypted_password = encrypt(password)
-	end
-    
-	def encrypt(string)
-		secure_hash("#{salt}--#{string}")
-	end
-    
-	def make_salt
-		secure_hash("#{Time.now.utc}--#{password}")
-	end
-    
-	def secure_hash(string)
-		Digest::SHA2.hexdigest(string)
-	end
 end
