d95c721de147b7f76e82657ed9adbc0fcaf98357
Showyilu <xiaoyi21b@hotmail.com>
2012-10-22 18:38:10 +0800

    Revert "Revert "add sesstion""
    This reverts commit 6eef0a92bdeb90def6bf9d8a1c97403475473c74.

--- picbook/app/controllers/application_controller.rb
+++ picbook/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+    include SessionsHelper
+    
+    
+	def current_user
+		@current = User.find(sessions[:user_id])
+	end
+    
+    protected
+  	
+	def authorize
+        unless User.find_by_id(session[:user_id])
+            redirect_to login_url, :alert => "Please log in"
+        end
+    end
+    protect_from_forgery
 end
--- picbook/app/models/user.rb
+++ picbook/app/models/user.rb
@@ -1,3 +1,4 @@
+require 'digest/sha2'
 class User < ActiveRecord::Base
     attr_accessible :birthday, :email, :firstName, :gender, :lastName, :password
     validates_confirmation_of :password
@@ -8,5 +9,30 @@ class User < ActiveRecord::Base
     validates :birthday, :email, :firstName,:lastName, :gender,:password,
     :presence => true
     validates :email, :uniqueness => true
-    validates_format_of  :email, :message => "has invalid format!", :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i   
+    validates_format_of  :email, :message => "has invalid format!", :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+    
+    before_save :encrypt_password
+    
+    def authenticate?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+    end
+    
+    private
+  	
+    def encrypt_password
+		self.salt = make_salt if new_record?
+		self.encrypted_password = encrypt(password)
+	end
+    
+	def encrypt(string)
+		secure_hash("#{salt}--#{string}")
+	end
+    
+	def make_salt
+		secure_hash("#{Time.now.utc}--#{password}")
+	end
+    
+	def secure_hash(string)
+		Digest::SHA2.hexdigest(string)
+	end
 end
