f1b01d7ef44ef5e0c8b23e4fb43f27f25318d87f
YongDing <dytack0815@gmail.com>
2012-10-22 22:59:34 +0800

    change password to hashpassword

--- picbook/app/models/user.rb
+++ picbook/app/models/user.rb
@@ -1,4 +1,4 @@
-
+require 'digest/sha2'
 class User < ActiveRecord::Base
     attr_accessible :birthday, :email, :firstName, :gender, :lastName, :password, :profile
     validates_confirmation_of :password
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
     attr_accessor :password
     
     has_many :pics
+    
     validates :birthday, :email, :firstName,:lastName, :gender,:password,
     :presence => true
     validates :email, :uniqueness => true
@@ -13,12 +14,36 @@ class User < ActiveRecord::Base
     
     
     
-    def User.authenticate?(submitted_password)
-		if("123" == submitted_password)
-            return true
-        else
-            return false
+    def User.authenticate(name, password)
+        if user = find_by_name(name)
+            if user.hashed_password == encrypt_password(password, user.salt)
+                user
+            end
         end
     end
     
+    def User.encrypt_password(password, salt)
+        Digest::SHA2.hexdigest(password + "wibble" + salt)
+    end
+    
+    # 'password' is a virtual attribute
+    def password=(password)
+        @password = password
+        
+        if password.present?
+            generate_salt
+            self.hashed_password = self.class.encrypt_password(password, salt)
+        end
+    end
+    
+    private
+    
+    def password_must_be_present
+        errors.add(:password, "Missing password") unless hashed_password.present?
+    end
+    
+    def generate_salt
+        self.salt = self.object_id.to_s + rand.to_s
+    end
+
 end
--- picbook/db/schema.rb
+++ picbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022113307) do
+ActiveRecord::Schema.define(:version => 20121022144709) do
 
   create_table "pics", :force => true do |t|
     t.string   "title"
@@ -29,10 +29,11 @@ ActiveRecord::Schema.define(:version => 20121022113307) do
     t.string   "gender"
     t.date     "birthday"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "profile"
+    t.string   "hashed_password"
+    t.string   "salt"
   end
 
 end
