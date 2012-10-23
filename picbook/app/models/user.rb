bdf3202148a6a2a96d350daa8c359725ba749e66
YongDing <dytack0815@gmail.com>
2012-10-23 20:20:26 +0800

    add avatar to user

--- picbook/app/models/user.rb
+++ picbook/app/models/user.rb
@@ -1,11 +1,20 @@
 require 'digest/sha2'
+
 class User < ActiveRecord::Base
+    
+    has_many :pics
+    
+    has_attached_file :profile, :dependent => :destroy
+    
     attr_accessible :birthday, :email, :firstName, :gender, :lastName, :password, :profile
+    
+    
+    
     validates_confirmation_of :password
     attr_accessible :password_confirmation
     attr_accessor :password
     
-    has_many :pics
+    
     
     validates :birthday, :email, :firstName,:lastName, :gender,:password,
     :presence => true
@@ -13,7 +22,7 @@ class User < ActiveRecord::Base
     validates_format_of  :email, :message => "has invalid format!", :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
     
     
-    
+
     def User.authenticate(email, password)
         if user = find_by_email(email)
             if user.hashed_password == encrypt_password(password, user.salt)
--- picbook/db/schema.rb
+++ picbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022144709) do
+ActiveRecord::Schema.define(:version => 20121023121729) do
 
   create_table "pics", :force => true do |t|
     t.string   "title"
@@ -29,11 +29,14 @@ ActiveRecord::Schema.define(:version => 20121022144709) do
     t.string   "gender"
     t.date     "birthday"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "profile"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.string   "hashed_password"
     t.string   "salt"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
