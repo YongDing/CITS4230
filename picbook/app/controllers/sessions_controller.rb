e992d647e7c95aba1d0639f13fadbadc281ee243
YongDing <dytack0815@gmail.com>
2012-10-24 21:12:59 +0800

    Add links of all pages

--- picbook/app/controllers/sessions_controller.rb
+++ picbook/app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     
     def destroy
         session[:user_id] = nil
-        redirect_to login_url
+        redirect_to :controller => 'picbook', :action => 'index'
     end
   
 end
