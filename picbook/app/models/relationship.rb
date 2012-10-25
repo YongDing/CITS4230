444f9a4b1e97fca22a5d910cd5ea46681a8ce4bb
Showyilu <xiaoyi21b@hotmail.com>
2012-10-25 12:11:43 +0800

    ddfdf

--- picbook/app/models/relationship.rb
+++ picbook/app/models/relationship.rb
@@ -1,3 +1,5 @@
 class Relationship < ActiveRecord::Base
   attr_accessible :friend_id, :person_id
+    belongs_to :person, :foreign_key => "person_id", :class_name => "User"
+    belongs_to :friend, :foreign_key => "friend_id", :class_name => "Person"
 end
